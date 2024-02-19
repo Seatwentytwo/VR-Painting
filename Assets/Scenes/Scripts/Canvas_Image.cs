336c8b31caeca5ee61f5488c3b8ac9b513b98e5c
Seatwentytwo <C22480762@mytudublin.ie>
2024-02-19 00:29:07 +0000

    Script Set Up

--- Assets/Scenes/Scripts/Canvas_Image.cs
+++ Assets/Scenes/Scripts/Canvas_Image.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tip_Color : MonoBehaviour
+public class Canvas_Image : MonoBehaviour
 {
+    Texture Image;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Image = Default;
     }
 
     // Update is called once per frame
