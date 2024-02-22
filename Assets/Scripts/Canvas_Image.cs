497099242b780639a5091134c4bf649b6bcf09e2
Seatwentytwo <C22480762@mytudublin.ie>
2024-02-22 14:55:13 +0000

    Audio

---
+++ Assets/Scripts/Canvas_Image.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas_Image : MonoBehaviour
+{
+    Texture Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Image = Default;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tip_Colour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tip_Color : MonoBehaviour
+{
+    bool blue;
+    bool red;
+    bool green;
+    bool yellow;
+    bool cyan;
+    bool pink;
+    bool orange;
+    bool thinner;
+
+    Material Tip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blue = false;
+        red = false;
+        green = false;
+        yellow = false;
+        cyan = false;
+        pink = false;
+        orange = false;
+        thinner = false;
+
+        //Tip = red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
