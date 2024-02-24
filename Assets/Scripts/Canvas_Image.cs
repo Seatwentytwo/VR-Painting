89dabd2922ddb0e1037b37c3cc53d2bb995d0136
Seatwentytwo <C22480762@mytudublin.ie>
2024-02-24 21:49:24 +0000

    Canvas Rework

--- Assets/Scripts/Canvas_Image.cs
+++ Assets/Scripts/Canvas_Image.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Canvas_Image : MonoBehaviour
 {
-    Material Image;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Image = GetComponent<Material>();
+
     }
 
     // Update is called once per frame
