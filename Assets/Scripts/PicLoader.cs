9f69e46a2ed7dce1304daed40d6d03f20731308f
Seatwentytwo <C22480762@mytudublin.ie>
2024-03-22 10:56:11 +0000

    In School Work

--- Assets/Scripts/PicLoader.cs
+++ Assets/Scripts/PicLoader.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PicLoader : MonoBehaviour
 {
+    public Texture2D Intro;
+    public Texture2D Tree;
+    public Texture2D Eye;
+    public Texture2D Bell;
+    public Texture2D Pizza;
+    public Texture2D Easel;
+    public Texture2D Triangle;
+    public Texture2D Moon;
+    public Texture2D End;
+
+    public GameObject ElapseEasel;
+
+    private EaselSwap ElapseScript;
+    private int TimerElapsed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ElapseScript = GetComponent<EaselSwap>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TimerElapsed = ElapseScript.TimerElapses;
+
+        if (TimerElapsed == 0)
+        {
+            //Intro;
+        }
     }
 }
